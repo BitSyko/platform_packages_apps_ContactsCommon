e07bae89a041fed38969d70e1a9eef8b1a40900b
camcory <camcory1@gmail.com>
2015-04-11 11:11:23 -0400

    Layers 2.1 (backwards compatible)
    This is a rework of the initial layers type 2 commit
    
    Changes naming conventions to coexist with AOSP/CMTE naming
    
    Original commit:
    Layers : Exposing hard coded resources for type 2 overlay access [4/6]
    Author: bgill55
    https://github.com/BitSyko/platform_packages_apps_ContactsCommon/commit/efcc85c54f46170f91714061bfe86aea75a1adbb
    
    Backwards compatibility based on
    [WIP] Resources: Layers 2 backwards-compatibility (Settings)
    Author: Andrew Dodd
    https://gerrit.omnirom.org/#/c/12695/
    
    CM commits:
    Exposed hard coded text colors for starred/group tiles.
    Author: 93Akkord
    https://github.com/CyanogenMod/android_packages_apps_ContactsCommon/commit/898ee9c8298e4a9a66d942ca33d068840fb2dc92
    
    Expose colors from array
    Author: djdarkknight96
    https://github.com/CyanogenMod/android_packages_apps_ContactsCommon/commit/0c375a8a833f122eb12da9a172b15216ebda4b1e
    
    ContactsCommon: Add optional text highlight color
    Author: cretin45
    https://github.com/CyanogenMod/android_packages_apps_ContactsCommon/commit/ffb6b0d7d1fe9e569bf570bc238eef99ad224a7a

--- src/com/android/contacts/common/list/ContactListItemView.java
+++ src/com/android/contacts/common/list/ContactListItemView.java
@@ -226,7 +226,9 @@ public class ContactListItemView extends ViewGroup
     public ContactListItemView(Context context) {
         super(context);
 
-        mTextHighlighter = new TextHighlighter(Typeface.BOLD);
+        mTextHighlighter = new TextHighlighter(Typeface.BOLD,
+                context.getResources().getColor(R.color.text_highlight_color));
+
         mNameHighlightSequence = new ArrayList<HighlightSequence>();
         mNumberHighlightSequence = new ArrayList<HighlightSequence>();
     }
@@ -279,7 +281,8 @@ public class ContactListItemView extends ViewGroup
                 a.getDimensionPixelOffset(
                         R.styleable.ContactListItemView_list_item_padding_bottom, 0));
 
-        mTextHighlighter = new TextHighlighter(Typeface.BOLD);
+        mTextHighlighter = new TextHighlighter(Typeface.BOLD,
+            context.getResources().getColor(R.color.text_highlight_color));
 
         a.recycle();
 
